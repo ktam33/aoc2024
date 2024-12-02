939d1e72dea9203a58eea1c9e0e3c23bd381efeb
Kenneth Tam <ktam33@gmail.com>
2024-12-02 16:47:07 -0600

    removed code

--- day2/day2.py
+++ day2/day2.py
@@ -1,15 +1,4 @@
 def is_safe(line):
-    numbers = [int(x) for x in line.split(' ')]
-    is_ascending = numbers[0] < numbers[1]
-    for i in range(len(numbers) - 1):
-        diff = numbers[i + 1] - numbers[i]
-        if is_ascending and diff < 0:
-            return False
-        if not is_ascending and diff > 0:
-            return False
-        abs_diff = abs(diff)
-        if abs_diff < 1 or abs_diff > 3:
-            return False 
     return True
 
 # read file contents into a list called lines
