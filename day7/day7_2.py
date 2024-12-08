2e83b439c26ebb8988cf010909998888254d6e4d
Kenneth Tam <ktam33@gmail.com>
2024-12-07 19:59:40 -0600

    day 7

--- day7/day7_2.py
+++ day7/day7_2.py
@@ -1,5 +1,6 @@
 from tqdm import tqdm
 import itertools
+import math
 
 # read file contents into a list called lines
 with open('input.txt', 'r') as file:
@@ -14,22 +15,22 @@ for line in lines:
     numbers.append([int(x) for x in line_parts[1].split(' ')])
 
 result = 0
-for i, total in enumerate(tqdm(answers)):
-    o = [['+', '*']] * (len(numbers[i]) - 1)
+for i, total in (enumerate(tqdm(answers))):
+    o = [['+', '*', '||']] * (len(numbers[i]) - 1)
     operators_list = list(itertools.product(*o))
     for operators in operators_list:
         total = numbers[i][0]
         for j in range(len(numbers[i]) - 1):
+            next_num = numbers[i][j + 1]
             if operators[j] == '+':
-                total += numbers[i][j + 1]
-            else:
-                total *= numbers[i][j + 1]
-                
+                total += next_num
+            elif operators[j] == '*':
+                total *= next_num
+            elif operators[j] == '||':
+                # total = int(str(total) + str(numbers[i][j + 1]))
+                total = total * (10 ** (int(math.log10(next_num)) + 1)) + next_num
         if total == answers[i]:
             result += total
             break
 
 print(result)
-                             
-
-
