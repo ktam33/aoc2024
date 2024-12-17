1a5f661d1853d74822f6f1dd7bbdbc50d575b382
Kenneth Tam <ktam33@gmail.com>
2024-12-17 08:32:59 -0600

    day15 finally

--- day15/day15_2.py
+++ day15/day15_2.py
@@ -1,187 +1,142 @@
-with open('test_input.txt', 'r') as file:
+is_wide = False
+with open('input.txt', 'r') as file:
+    if 'wide' in file.name:
+        is_wide = True
     lines = [line.strip() for line in file]
 
 def print_grid(grid):
     for line in grid:
         print("".join(line))
 
-def move(pos, dir, grid, h, w):
+def move(pos, dir, grid, h, w, do_move = True):
     curr = grid[pos[0]][pos[1]]
-    print(f'{curr}: {pos}, {dir}')
+    if curr not in '@[]':
+        raise Exception('Moving illegal character')
+    # print(f'{curr}: {pos}, {dir}')
+    # if requested to move out of the grid, return current position
     if ((dir == '>' and pos[1] == w - 1) or 
         (dir == '<' and pos[1] == 0) or
         (dir == 'v' and pos[0] == h - 1) or
         (dir == '^' and pos[0] == 0)):
         return pos
     if dir == '>':
-        next = grid[pos[0]][pos[1] + 1]
-        if next == '.':
-            grid[pos[0]][pos[1] + 1] = curr
-            grid[pos[0]][pos[1]] = '.'
-            return (pos[0], pos[1] + 1)
-        elif next == '#':
-            return pos
-        elif next == '[' or next == ']':
-            move_next = move((pos[0], pos[1] + 1), dir, grid, h, w)
-            if move_next != (pos[0], pos[1] + 1):
-                grid[pos[0]][pos[1] + 1] = curr
-                grid[pos[0]][pos[1]] = '.'
-                return (pos[0], pos[1] + 1)
-            else:
-                return pos
+        return move_horizontally(pos, dir, grid)
     elif dir == '<': 
-        next = grid[pos[0]][pos[1] - 1]
-        if next == '.':
-            grid[pos[0]][pos[1] - 1] = curr
-            grid[pos[0]][pos[1]] = '.'
-            return (pos[0], pos[1] - 1)
-        elif next == '#':
-            return pos
-        elif next == '[' or next == ']':
-            move_next = move((pos[0], pos[1] - 1), dir, grid, h, w)
-            if move_next != (pos[0], pos[1] - 1):
-                grid[pos[0]][pos[1] - 1] = curr
-                grid[pos[0]][pos[1]] = '.'
-                return (pos[0], pos[1] - 1)
-            else:
-                return pos
+        return move_horizontally(pos, dir, grid)
     elif dir == 'v':
-        next = grid[pos[0] + 1][pos[1]]
-        if next == '.' and curr == '@':
-            grid[pos[0] + 1][pos[1]] = curr
-            grid[pos[0]][pos[1]] = '.'
-            return (pos[0] + 1, pos[1])
-        elif next == '.' and curr == '[':
-            if grid[pos[0] + 1][pos[1] + 1] == '.':
-                grid[pos[0]][pos[1]] = '.'
-                grid[pos[0] + 1][pos[1]] = '['
-                grid[pos[0]][pos[1] + 1] = '.'
-                grid[pos[0] + 1][pos[1] + 1] = ']'
-                return (pos[0] + 1, pos[1])
-            elif grid[pos[0] + 1][pos[1] + 1] == '#':
-                return pos
-            elif grid[pos[0] + 1][pos[1] + 1] == '[' or grid[pos[0] + 1][pos[1] + 1] == ']':
-                move_next = move((pos[0] + 1, pos[1] + 1), dir, grid, h, w)
-                if move_next != (pos[0] + 1, pos[1] + 1):
-                    grid[pos[0]][pos[1]] = '.'
-                    grid[pos[0] + 1][pos[1]] = '['
-                    grid[pos[0]][pos[1] + 1] = '.'
-                    grid[pos[0] + 1][pos[1] + 1] = ']'
-                    return (pos[0] + 1, pos[1])
-        elif next == '.' and curr == ']':
-            if grid[pos[0] + 1][pos[1] - 1] == '.':
-                grid[pos[0]][pos[1]] = '.'
-                grid[pos[0] + 1][pos[1]] = ']'
-                grid[pos[0]][pos[1] - 1] = '.'
-                grid[pos[0] + 1][pos[1] - 1] = '['
-                return (pos[0] + 1, pos[1])
-            elif grid[pos[0] + 1][pos[1] - 1] == '#':
-                return pos
-            elif grid[pos[0] + 1][pos[1] - 1] == '[' or grid[pos[0] + 1][pos[1] - 1] == ']':
-                move_next = move((pos[0] + 1, pos[1] - 1), dir, grid, h, w)
-                if move_next != (pos[0] + 1, pos[1] - 1):
-                    grid[pos[0]][pos[1]] = '.'
-                    grid[pos[0] + 1][pos[1]] = '['
-                    grid[pos[0]][pos[1] - 1] = '.'
-                    grid[pos[0] + 1][pos[1] - 1] = ']'
-                    return (pos[0] + 1, pos[1])
-        elif next == '#':
-            return pos
-        elif next == '[' or next == ']':
-            move_next = move((pos[0] + 1, pos[1]), dir, grid, h, w)
-            if next == '[':
-                move((pos[0] + 1, pos[1] + 1), dir, grid, h, w)
-            if next == ']':
-                move((pos[0] + 1, pos[1] - 1), dir, grid, h, w)
-            if move_next != (pos[0] + 1, pos[1]):
-                grid[pos[0] + 1][pos[1]] = curr
-                grid[pos[0]][pos[1]] = '.'
-                return (pos[0] + 1, pos[1])
-            else:
-                return pos
+        return move_vertically(pos, dir, grid)
     elif dir == '^':
-        next = grid[pos[0] - 1][pos[1]]
-        if next == '.' and curr == '@':
-            grid[pos[0] - 1][pos[1]] = curr
-            grid[pos[0]][pos[1]] = '.'
-            return (pos[0] - 1, pos[1])
-        elif next == '.' and curr == '[':
-            if grid[pos[0] - 1][pos[1] + 1] == '.':
-                grid[pos[0]][pos[1]] = '.'
-                grid[pos[0] - 1][pos[1]] = '['
-                grid[pos[0]][pos[1] + 1] = '.'
-                grid[pos[0] - 1][pos[1] + 1] = ']'
-                return (pos[0] + 1, pos[1])
-            elif grid[pos[0] - 1][pos[1] + 1] == '#':
-                return pos
-            elif grid[pos[0] - 1][pos[1] + 1] == '[' or grid[pos[0] - 1][pos[1] + 1] == ']':
-                move_next = move((pos[0] - 1, pos[1] + 1), dir, grid, h, w)
-                if move_next != (pos[0] - 1, pos[1] + 1):
-                    grid[pos[0]][pos[1]] = '.'
-                    grid[pos[0] - 1][pos[1]] = '['
-                    grid[pos[0]][pos[1] + 1] = '.'
-                    grid[pos[0] - 1][pos[1] + 1] = ']'
-                    return (pos[0] - 1, pos[1])
-        elif next == '.' and curr == ']':
-            if grid[pos[0] - 1][pos[1] - 1] == '.':
-                grid[pos[0]][pos[1]] = '.'
-                grid[pos[0] - 1][pos[1]] = ']'
-                grid[pos[0]][pos[1] - 1] = '.'
-                grid[pos[0] - 1][pos[1] - 1] = '['
-                return (pos[0] - 1, pos[1])
-            elif grid[pos[0] - 1][pos[1] - 1] == '#':
-                return pos
-            elif grid[pos[0] - 1][pos[1] - 1] == '[' or grid[pos[0] - 1][pos[1] - 1] == ']':
-                move_next = move((pos[0] - 1, pos[1] - 1), dir, grid, h, w)
-                if move_next != (pos[0] - 1, pos[1] - 1):
-                    grid[pos[0]][pos[1]] = '.'
-                    grid[pos[0] - 1][pos[1]] = '['
-                    grid[pos[0]][pos[1] - 1] = '.'
-                    grid[pos[0] - 1][pos[1] - 1] = ']'
-                    return (pos[0] - 1, pos[1])
-        elif next == '#':
-            return pos
-        elif next == '[' or next == ']':
-            move_next = move((pos[0] - 1, pos[1]), dir, grid, h, w)
-            if next == '[':
-                move((pos[0] - 1, pos[1] + 1), dir, grid, h, w)
-            if next == ']':
-                move((pos[0] - 1, pos[1] - 1), dir, grid, h, w)
-            if move_next != (pos[0] - 1, pos[1]):
-                grid[pos[0] - 1][pos[1]] = curr
-                grid[pos[0]][pos[1]] = '.'
-                return (pos[0] - 1, pos[1])
-            else:
-                return pos
+        return move_vertically(pos, dir, grid)
             
+def move_horizontally(pos, dir, grid):
+    next_pos = get_next(pos, dir)
+    next = grid[next_pos[0]][next_pos[1]]
+    if next == '.':
+        return make_move(pos, dir, grid)
+    elif next == '#':
+        return pos
+    elif next == '[' or next == ']':
+        move_next = move(next_pos, dir, grid, h, w)
+        if move_next != next_pos:
+            return make_move(pos, dir, grid)
+        else:
+            return pos
+            
+def move_vertically(pos, dir, grid):
+    next_pos = get_next(pos, dir)
+    next = grid[next_pos[0]][next_pos[1]]
+    if next == '.':
+        return make_move(pos, dir, grid)
+    elif '#' in next:
+        return pos
+    elif next == '[':
+        pos_list = set([next_pos, get_next(next_pos, '>')])
+        if move_all(pos_list, dir, grid):
+            return make_move(pos, dir, grid)
+        else:
+            return pos
+    elif next == ']':
+        pos_list = set([next_pos, get_next(next_pos, '<')])
+        if move_all(pos_list, dir, grid):
+            return make_move(pos, dir, grid)
+        else:
+            return pos
+        
+def make_move(pos, dir, grid):
+    curr = grid[pos[0]][pos[1]]
+    new_pos = get_next(pos, dir)
+    grid[new_pos[0]][new_pos[1]] = curr
+    grid[pos[0]][pos[1]] = '.'
+    return new_pos
+
+def move_all(pos_list, dir, grid):
+    next_pos_list = set()
+    for pos in pos_list:
+        next_pos= get_next(pos, dir)
+        next_pos_value = grid[next_pos[0]][next_pos[1]]
+        if next_pos_value == '#':
+            return False
+        elif next_pos_value == '.':
+            pass
+        elif next_pos_value == ']':
+            next_pos_list.add(next_pos)
+            next_pos_list.add(get_next(next_pos, '<'))
+        elif next_pos_value == '[':
+            next_pos_list.add(next_pos)
+            next_pos_list.add(get_next(next_pos, '>'))
+    if len(next_pos_list) == 0:
+        for pos in pos_list:
+            make_move(pos, dir, grid)
+        return True
+    else:
+        if move_all(next_pos_list, dir, grid):
+            for pos in pos_list:
+                make_move(pos, dir, grid)
+            return True
+        else:
+            return False
+
+def get_next(pos, dir):
+    if dir == '^':
+        return (pos[0] - 1, pos[1])
+    elif dir == 'v':
+        return (pos[0] + 1, pos[1])
+    elif dir == '>':
+        return (pos[0], pos[1] + 1)
+    elif dir == '<':
+        return (pos[0], pos[1] - 1)
 
 h = lines.index('')
-w = len(lines[0]) * 2
+input_w = len(lines[0])
 robot = (0,0)
+w = input_w * 2 if not is_wide else input_w
 grid = [['.'] * w for _ in range(h)]
 for i in range(h):
-    for j in range(int(w/2)):
-        if lines[i][j] == '.':
-            grid[i][j * 2: j * 2 + 2] = ".."
-        if lines[i][j] == '#':
-            grid[i][j * 2: j * 2 + 2] = "##"
-        if lines[i][j] == 'O':
-            grid[i][j * 2: j * 2 + 2] = "[]"
-        if lines[i][j] == '@':
-            grid[i][j * 2: j * 2 + 2] = "@."
-            robot = (i, j * 2)
-
+    for j in range(input_w):
+        if not is_wide:
+            if lines[i][j] == '.':
+                grid[i][j * 2: j * 2 + 2] = ".."
+            if lines[i][j] == '#':
+                grid[i][j * 2: j * 2 + 2] = "##"
+            if lines[i][j] == 'O':
+                grid[i][j * 2: j * 2 + 2] = "[]"
+            if lines[i][j] == '@':
+                grid[i][j * 2: j * 2 + 2] = "@."
+                robot = (i, j * 2)
+        else:
+            grid[i][j] = lines[i][j]
+            if lines[i][j] == '@':
+                robot = (i, j)
 input_lines = []
 for i in range(h + 1, len(lines)):
     input_lines.append(lines[i].strip())
 
 inputs = "".join(input_lines)
 
-print_grid(grid)
+# print_grid(grid)
 for char in inputs:
     robot = move(robot, char, grid, h, w)
-    print_grid(grid)
-    input()
+    # print_grid(grid)
 
 answer = 0
 for i in range(h):
