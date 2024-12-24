462adef5810b44cd16da184df2b2d926e94e8f3f
Kenneth Tam <ktam33@gmail.com>
2024-12-23 18:41:20 -0600

    day 22

--- day22/day22_2.py
+++ day22/day22_2.py
@@ -1,4 +1,12 @@
 import math
+from tqdm import tqdm
+
+def get_combos():
+    for i in range(-9, 10):
+        for j in range(-9, 10):
+            for k in range(-9, 10):
+                for l in range(-9, 10):
+                    yield (i,j,k,l)
 
 def get_next_secret(number):
     prune_num = 16777216
@@ -6,23 +14,42 @@ def get_next_secret(number):
     b = (math.floor(a/32) ^ a) % prune_num
     return ((b * 2048) ^ b) % prune_num
 
+def get_prices(number):
+    curr_secrets = []
+    curr_prices = {}
+    curr_secrets.append((number, number % 10, 0))
+    for i in range(1, 2000):
+        number = get_next_secret(number)
+        price = number % 10
+        curr_secrets.append((number, price, price - curr_secrets[i - 1][1]))
+        if i > 3:
+            key = (
+                curr_secrets[i - 3][2],
+                curr_secrets[i - 2][2],
+                curr_secrets[i - 1][2],
+                curr_secrets[i][2],
+            )
+            if key not in curr_prices:
+                curr_prices[key] = price
+    return curr_prices
+
 with open('input.txt', 'r') as file:
     secrets = [int(line.strip()) for line in file]
 
-answer = 0
-buyer = []
+secret_prices = {}
 for secret in secrets:
-    curr_secrets = []
-    curr_secrets.add(secret, secret % 10)
-    for i in range(2000):
-        secret = get_next_secret(secret)
-        price = secret % 10
-        curr_secrets.add(secret, price)
-        if i > 3 == 0:
-            key = (
-                curr_secrets[i - ]
-            )
+    secret_prices[secret] = get_prices(secret)
 
-    answer += secret
+max_bananas = 0
+curr_combo = None
+for combo in tqdm(list(get_combos())):
+    curr_bananas = 0
+    for secret in secret_prices:
+        if combo in secret_prices[secret]:
+            curr_bananas += secret_prices[secret][combo]
+    if curr_bananas > max_price:
+        max_price = max_bananas
+        curr_combo = combo
 
-print(answer)
+print(curr_combo)
+print(max_price)
