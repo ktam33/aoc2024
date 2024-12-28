7e117c621d2f539b8da4785edbcf72ca21fbdb54
Kenneth Tam <ktam33@gmail.com>
2024-12-28 13:16:47 -0600

    day25

--- day25/day25.py
+++ day25/day25.py
@@ -1,7 +1,30 @@
-with open('test_input.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     lines = [line.strip() for line in file]
 
 keys = []
 locks = []
 
-for 
+len(lines)
+
+
+for i in range(0, len(lines), 8):
+    item = [0,0,0,0,0]
+    for j in (range(1, 6)):
+        line = lines[i + j]
+        for k in range(len(line)):
+            if line[k] == '#':
+                item[k] += 1
+    if lines[i] == '#####':
+        locks.append(item)
+    else:
+        keys.append(item)
+
+answer = 0
+for lock in locks:
+    for key in keys:
+        if all([lock[i] + key[i] <= 5 for i in range(5)]):
+            answer += 1
+
+print(locks)
+print(keys)
+print(answer)
